56a409f2b8f5750de4081f5808d244742e319484
Dev-krishnendu1 <krishnendumaity2110@gmail.com>
2025-08-03 10:44:37 +0530

    commit next

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,14 +16,18 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -5,9 +5,9 @@ import { config } from "dotenv";
 config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLAUDINARY_CLOUD_NAME,
+  api_key: process.env.CLAUDINARY_API_KEY,
+  api_secret: process.env.CLAUDINARY_API_SECRET,
 });
 
 export default cloudinary;
